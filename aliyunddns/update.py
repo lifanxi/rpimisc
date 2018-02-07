4e69f747fe4ca77bf0457c8e0076fa83c58b71ed
Li Fanxi <lifanxi@hotmail.com>
2018-02-07 10:38:28 +0800

    Fix Aliyun script

--- aliyunddns/update.py
+++ aliyunddns/update.py
@@ -1,27 +1,78 @@
-#! /usr/bin/env python2.7
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import aliyun.api
 import json
+import os
+import re
 import sys
-import urllib2
+from datetime import datetime
 import time
 
-def init_api():
-    # Setup Aliyun API Access key
-    conf = json.load(file('accesskey.json'))
-    aliyun.setDefaultAppInfo(str(conf['id']),str(conf['secret']))
+from aliyunsdkcore import client
+from aliyunsdkalidns.request.v20150109 import DescribeDomainRecordsRequest
+from aliyunsdkalidns.request.v20150109 import DescribeDomainRecordInfoRequest
+from aliyunsdkalidns.request.v20150109 import UpdateDomainRecordRequest
 
-def get_domain_record(record):
-    # Query domain record Id
-    r = aliyun.api.dns.DnsDescribeDomainRecordInfoRequest()
-    r.RecordId = record
-    try:
-        response = r.getResponse()
-        return response['Value']
-    except Exception, e:
-        print("get_domain_record:", e)
-        return None
-    
+# Setup Aliyun API Access key
+conf = json.load(file('accesskey.json'))
+
+# 阿里云 Access Key ID
+access_key_id = str(conf['id'])
+# 阿里云 Access Key Secret
+access_key_secret = str(conf['secret'])
+# 返回内容格式
+rc_format = 'json'
+
+"""
+获取域名的解析信息
+"""
+def check_records(dns_domain):
+    clt = client.AcsClient(access_key_id, access_key_secret, 'cn-hangzhou')
+    request = DescribeDomainRecordsRequest.DescribeDomainRecordsRequest()
+    request.set_DomainName(dns_domain)
+    request.set_accept_format(rc_format)
+    result = clt.do_action(request)
+    result = json.JSONDecoder().decode(result)
+    return result
+
+"""
+根据域名解析记录ID查询上一次的IP记录
+"""
+def get_old_ip(record_id):
+    clt = client.AcsClient(access_key_id,access_key_secret,'cn-hangzhou')
+    request = DescribeDomainRecordInfoRequest.DescribeDomainRecordInfoRequest()
+    request.set_RecordId(record_id)
+    request.set_accept_format(rc_format)
+    result = clt.do_action(request)
+    result = json.JSONDecoder().decode(result)
+    result = result['Value']
+    return result
+
+"""
+更新阿里云域名解析记录信息
+"""
+def update_dns(dns_rr, dns_type, dns_value, dns_record_id, dns_ttl, dns_format):
+    clt = client.AcsClient(access_key_id, access_key_secret, 'cn-hangzhou')
+    request = UpdateDomainRecordRequest.UpdateDomainRecordRequest()
+    request.set_RR(dns_rr)
+    request.set_Type(dns_type)
+    request.set_Value(dns_value)
+    request.set_RecordId(dns_record_id)
+    request.set_TTL(dns_ttl)
+    request.set_accept_format(dns_format)
+    result = clt.do_action(request)
+    return result
+
+"""
+通过 ip.cn 获取当前主机的外网IP
+"""
+def get_my_ip_public():
+    get_ip_method = os.popen('curl -s https://freemindworld.com/tools/myip.php')
+    get_ip_responses = get_ip_method.readlines()[0]
+    get_ip_pattern = re.compile(r'\d+\.\d+\.\d+\.\d+')
+    get_ip_value = get_ip_pattern.findall(get_ip_responses)
+    return get_ip_value
+
 def get_my_ip_internal():
     import socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -29,33 +80,6 @@ def get_my_ip_internal():
     ip = s.getsockname()[0] 
     s.close()
     return ip
-
-def get_my_ip():
-    try:
-        u = urllib2.urlopen('https://www.freemindworld.com/tools/myip.php')
-        return u.read().strip('\n')
-    except HTTPError as e:
-        print('getMyIp:',e)
-        return None
-
-def update_domain_record_id(record_id, ip, record):
-    r = aliyun.api.dns.DnsUpdateDomainRecordRequest()
-    r.RecordId = record_id
-    r.RR = record
-    r.Type = "A"
-    r.Value = ip
-    try:
-        response = r.getResponse()
-        if 'Code' in response:
-            print("get_domain_record:", response)
-            return False
-        else:
-            return True
-    except Exception, e:
-        print("get_domain_record:", e)
-        return False
-        
-    return True
 
 def get_cached_ip():
     try:
@@ -82,36 +106,51 @@ def update_cache(ip):
         print(e)
         pass
         
+
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print("Usage: ./update.py <Record Id> <record> <IP>")
+        print("Usage: ./update.py <top domain> <record> <IP>")
         sys.exit(-1)
-    record_id = sys.argv[1]
-    record = sys.argv[2]
+
+    rc_domain = sys.argv[1]
+    rc_record = sys.argv[2]
 
     if len(sys.argv) == 4:
-        ip = sys.argv[3]
-        if ip == "internal":
-            ip = get_my_ip_internal()
+        now_ip = sys.argv[3]
+        print now_ip
+        if now_ip == "internal":
+            now_ip = get_my_ip_internal()
     else:
-        ip = get_my_ip()
+        now_ip = get_my_ip_public()[0]
 
-    if get_cached_ip() == ip:
+    if get_cached_ip() == now_ip:
         print("No need to update. (Cached)")
         sys.exit(0)
         
-    init_api()
-    
-    if get_domain_record(record_id) == ip:
-        update_cache(ip)
-        print("No need to update")
-        sys.exit(0)
+    # # 之前的解析记录
+    old_ip = ""
+    record_id = ""
+    dns_records = check_records(rc_domain)
+    #print dns_records
+    for record in dns_records["DomainRecords"]["Record"]:
+        # print record["Type"] + "." + record["RR"]
+        if record["Type"] == 'A' and record["RR"] == rc_record:
+            record_id = record["RecordId"]
+            print "%s.%s recordID is %s" % (rc_record,rc_domain,record_id)
+            if record_id != "":
+                old_ip = get_old_ip(record_id)
+    # 获取主机当前的IP
+    print "now host ip is %s, dns ip is %s" % (now_ip, old_ip)
 
-    if update_domain_record_id(record_id, ip, record):
-        update_cache(ip)
-        print("Success")
-        sys.exit(0)
+    if old_ip == now_ip:
+        update_cache(now_ip)
+        print "No need to update. "
     else:
-        print("Failed")
-        sys.exit(-1)
+        rc_rr = rc_record           # 解析记录
+        rc_type = 'A'               # 记录类型, DDNS填写A记录
+        rc_value = now_ip           # 新的解析记录值
+        rc_record_id = record_id    # 记录ID
+        rc_ttl = '1000'             # 解析记录有效生存时间TTL,单位:秒
 
+        print update_dns(rc_rr, rc_type, rc_value, rc_record_id, rc_ttl, rc_format)
+        update_cache(now_ip)
